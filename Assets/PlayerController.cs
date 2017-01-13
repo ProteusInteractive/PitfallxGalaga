f30ca0b294c4381abe91c397ed3fc8902d2cc13a
ChiefTripodBear <avalacos@gmail.com>
2017-01-13 13:49:52 +1100

    Stopping work on Boundaries for now
    Movement code was ok but bounding box that scales to perspective camera.

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,52 +1,55 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
+    //public float speed;
+    //public GameObject player;
+    //public GameObject background;
 
-    public float speed;
-    public GameObject player;
-    Vector3 newPos;
-    float minimumX;
-    float minimumZ;
-    float maximumX;
-    float maximumZ;
+    //private float camDistance;
+    //private Vector3 botleft;
+    //private Vector3 botright;
+    //private Vector3 topleft;
+    //private Vector3 topright;
 
-    // Use this for initialization
-    void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    //// Use this for initialization
+    //void Start()
+    //{
+    //    camDistance = Camera.main.transform.position.y - background.transform.position.y;
+    //    botleft = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, camDistance));
+    //    botright = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, camDistance));
+    //    topleft = Camera.main.ViewportToWorldPoint(new Vector3(0, 1, camDistance));
+    //    topright = Camera.main.ViewportToWorldPoint(new Vector3(1, 1, camDistance));
 
-    void FixedUpdate()
-    {
-        //every frame before rendering, calculate player movement and constrain within movement boundaries
-        UpdatePlayerBounds();
-        PlayerMove();
-    }
+    //    print(botleft);
+    //    print(botright);
+    //    print(topleft);
+    //    print(topright);
 
-     void UpdatePlayerBounds()
+    //}
 
-    {
-        //get the distance from camera to player
-        float camDistance = Camera.main.transform.position.z - player.transform.position.z;
-        //print screen position of player each frame based on camera position
-        Vector3 p = Camera.main.ScreenToWorldPoint(new Vector3 (player.transform.position.x, player.transform.position.y, camDistance));
-        print(p);
-        Vector3 q = Camera.main.ScreenToWorldPoint(new Vector3());
-    }
+    //void Update()
+    //{
+    //    //every frame before rendering, calculate player movement and constrain within movement boundaries
+    //    PlayerMove();
+    //}
 
-    void PlayerMove()
-    {
-        //get frame by frame movement
-        newPos.x += Input.GetAxis("P1_Horizontal") * speed;
-        newPos.z += Input.GetAxis("P1_Vertical") * speed;
-        //clamp to viewport
-        newPos.x = Mathf.Clamp(newPos.x, minimumX, maximumX);
-        newPos.z = Mathf.Clamp(newPos.z, minimumZ, maximumZ);
-        player.transform.localPosition = newPos;
-    }
+    
+
+    //void PlayerMove()
+    //{
+    //    float xMove = Input.GetAxis("P1_Horizontal") * speed * Time.deltaTime;
+    //    float zMove = Input.GetAxis("P1_Vertical") * speed * Time.deltaTime;
+    //    Vector3 newPos = new Vector3(player.transform.position.x + xMove, player.transform.position.y, player.transform.position.z + zMove);
+
+    //    //check player position inside the bounding box
+    //    //if ()
+    //    //{
+    //    //    if (zmin < newZPos && newZPos < zmax)
+    //    //    {
+    //    //        player.transform.Translate(xMove, 0, zMove);
+    //    //    }
+    //    //}
+    //}
 }
