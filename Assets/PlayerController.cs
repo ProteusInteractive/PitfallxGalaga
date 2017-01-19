a6e60ae5a1464e2754a1d79209b046dc5ea11171
ChiefTripodBear <avalacos@gmail.com>
2017-01-20 00:00:10 +1100

    redid movement and boundaries to use physics
    player movement is done via adding force in physics engine.  boundaries
    are enforced with bounding box.  drag applied to player to slow force.
    cover created but they don't move or instantiate.

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -3,8 +3,9 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour
 {
-    //public float speed;
-    //public GameObject player;
+    public float speed;
+    public GameObject player;
+    Rigidbody rb;
     //public GameObject background;
 
     //private float camDistance;
@@ -13,43 +14,36 @@ public class PlayerController : MonoBehaviour
     //private Vector3 topleft;
     //private Vector3 topright;
 
-    //// Use this for initialization
-    //void Start()
-    //{
-    //    camDistance = Camera.main.transform.position.y - background.transform.position.y;
-    //    botleft = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, camDistance));
-    //    botright = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, camDistance));
-    //    topleft = Camera.main.ViewportToWorldPoint(new Vector3(0, 1, camDistance));
-    //    topright = Camera.main.ViewportToWorldPoint(new Vector3(1, 1, camDistance));
+    // Use this for initialization
+    void Start()
+    {
+        //camDistance = Camera.main.transform.position.y - background.transform.position.y;
+        //botleft = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, camDistance));
+        //botright = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, camDistance));
+        //topleft = Camera.main.ViewportToWorldPoint(new Vector3(0, 1, camDistance));
+        //topright = Camera.main.ViewportToWorldPoint(new Vector3(1, 1, camDistance));
 
-    //    print(botleft);
-    //    print(botright);
-    //    print(topleft);
-    //    print(topright);
+        //print(botleft);
+        //print(botright);
+        //print(topleft);
+        //print(topright);
 
-    //}
+        rb = GetComponent<Rigidbody>();
 
-    //void Update()
-    //{
-    //    //every frame before rendering, calculate player movement and constrain within movement boundaries
-    //    PlayerMove();
-    //}
+    }
 
-    
+    void Update()
+    {
+        //every frame before rendering, calculate player movement and constrain within movement boundaries
+        PlayerMove();
+    }
 
-    //void PlayerMove()
-    //{
-    //    float xMove = Input.GetAxis("P1_Horizontal") * speed * Time.deltaTime;
-    //    float zMove = Input.GetAxis("P1_Vertical") * speed * Time.deltaTime;
-    //    Vector3 newPos = new Vector3(player.transform.position.x + xMove, player.transform.position.y, player.transform.position.z + zMove);
 
-    //    //check player position inside the bounding box
-    //    //if ()
-    //    //{
-    //    //    if (zmin < newZPos && newZPos < zmax)
-    //    //    {
-    //    //        player.transform.Translate(xMove, 0, zMove);
-    //    //    }
-    //    //}
-    //}
+
+    void PlayerMove()
+    {
+        float xMove = Input.GetAxis("P1_Horizontal") * speed * Time.deltaTime;
+        float zMove = Input.GetAxis("P1_Vertical") * speed * Time.deltaTime;
+        rb.AddForce(xMove, 0, zMove);
+    }
 }
